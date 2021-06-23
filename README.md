66580be67c20a66e50d18fd65cfc0d834bbcee86
tomieiro <matheustomieiro@usp.br>
2021-06-23 14:47:15 -0300

    updating and adding libs

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Lua Notebook
 Lua notebook prototype that supports comments.
+
+<h5>BEFORE YOU START:</h5>
+<br>
+<ul>
+<li><em><strong>(STEP 1)</strong></em></p>
+Initially, some libs are needed: liblua5.3-dev(https://www.lua.org/), libfltk1.3-dev(https://www.fltk.org/) and the lua binaries themselves(https ://www.lua.org/).<br><p>To install them, if you are on Linux, use: <code>make INSTALL_COMPONENTS</p></code>
+</li>
+<br><br>
+<li>
+<p><em><strong>(STEP 2)</strong></em></p>
+For the GUI in Lua, we use the lib: MoonFLTK(https://github.com/stetre/moonfltk) by Stefano Trettel. Access the moonfltk link, mentioned above, to learn about the module. The walkthrough is on the repository page if you want to compile it; however, it will already be installed by <code>make INSTALL_COMPONENTS</code>(Comming soon...).
+</li>
+</ul>
+<br><br>
+
--- notebook.lua
+++ notebook.lua
@@ -1,8 +1,38 @@
 local runtime = require "runtime"
+
+package.cpath = "/usr/local/lib/lua/5.3/?.so;" .. package.cpath;
+local fl = require "moonfltk";
+
+function Gui()
+    --JANELA PRINCIPAL
+    x,y,w,h = fl.screen_xywh()
+    JANELA = fl.double_window(1300,600,"Lua Notebook")
+    JANELA:color(fl.DARK3)
+    --JANELA:fullscreen()
+
+    --INTERFACE
+    fl.label(370, 5, 595, 30,"Tabalho 1");
+    fl.round_clock(1230, 5, 35, 30);
+    local scroll = fl.scroll(0, 40, 1360, 665);
+    scroll:color(fl.DARK3);
+    for i=1, 10 do
+        scroll:add(fl.input(60, 70+(i*100), 1115, 50));
+        scroll:add(fl.button(1195, 130+(i*100), 90, 25, "Run"));
+        scroll:add(fl.input(60, 130+(i*100), 1115, 25, "stdout"));
+    end
+end
+
+Gui();
+JANELA:done()
+JANELA:show()
+
+
 print(runtime.run_code(runtime.str_to_function(
     "function b()\
         print('ok');\
-        return 2;\
+        return 0;\
     end"
     )
 ));
+
+return fl:run()
