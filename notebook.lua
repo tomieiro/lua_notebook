43ac51322f573d618b2914a8d84171562276a1cd
tomieiro <matheustomieiro@usp.br>
2021-07-25 20:47:22 -0300

    Updating

--- notebook.lua
+++ notebook.lua
@@ -3,6 +3,15 @@ local runtime = require "runtime"
 package.cpath = "/usr/local/lib/lua/5.3/?.so;" .. package.cpath;
 local fl = require "moonfltk";
 
+local itens_count = 1
+local offset = 120;
+local offset_scroll;
+
+function UpdateGui()
+    scroll:redraw();
+    window:redraw();
+end
+
 function RunCallback(_, args)
     local widget = args[2];
     local command = args[1]:value();
@@ -12,43 +21,55 @@ function RunCallback(_, args)
         widget:textcolor(fl.YELLOW);
         local file = io.open("stdout.temp","r+");
         local buffer = file:read("*a");
-        print(buffer)
         widget:value(buffer);
         file:close();
     elseif(not(type(response) == "nil" or type(response) == "boolean")) then
         widget:textcolor(fl.GREEN);
         widget:value(response);
+    elseif(type(response) == "boolean") then
+        widget:textcolor(fl.MAGENTA);
+        widget:value("OK!");
     else
         widget:value("error");
         widget:textcolor(fl.RED);
     end
+end
 
+function Item()
+    itens_count = itens_count + 1;
+    group = fl.group(1255, 80+(offset*itens_count)-offset_scroll, 1100, 50);
+    comments = fl.input(30, 80+(offset*itens_count)-offset_scroll, 1100, 50); --Comentarios group_n = 1
+    group:add(comments);
+    code = fl.input(30, 130+(offset*itens_count)-offset_scroll, 1100, 25); --Codigo group_n = 2
+    group:add(code);
+    output = fl.output(30, 130+(offset*itens_count)+25-offset_scroll, 1100, 25); --Saida group_n = 3
+    output:color(fl.DARK_BLUE);
+    output:textfont(fl.BOLD);
+    group:add(output);
+    runButton = fl.button(1150, 130+(offset*itens_count)-offset_scroll, 90, 25, "Run"); --Botao group_n = 4
+    runButton:callback(RunCallback,{group:child(2),group:child(3)});
+    group:add(runButton);
+    scroll:add(comments); scroll:add(code); scroll:add(output); scroll:add(runButton);
+    UpdateGui();
+    offset_scroll = scroll:yposition()+250;
 end
 
 function Gui()
-    x,y,w,h = fl.screen_xywh()
+    local x,y,w,h = fl.screen_xywh()
+    itens_count = 1;
+    offset_scroll = 250
     window = fl.double_window(1300,600,"Lua Notebook")
     window:color(fl.DARK3)
     --window:fullscreen()
     fl.round_clock(1230, 5, 35, 30);
-    local scroll = fl.scroll(0, 40, 1360, 665);
+    scroll = fl.scroll(20, 40, 1250, 500);
     scroll:color(fl.DARK3);
-    local comments, code, runButton, output;
-    local group;
-    local count;
-    count =120;
-    group = fl.group(1255, 80+count, 1115, 50);
-    comments = fl.input(60, 80+count, 1115, 50); --Comentarios group_n = 1
-    group:add(comments);
-    code = fl.input(60, 130+count, 1115, 25); --Codigo group_n = 2
-    group:add(code);
-    output = fl.output(60, 130+count+25, 1115, 25); --Saida group_n = 3
-    output:color(fl.DARK2);
-    group:add(output);
-    runButton = fl.button(1195, 130+count, 90, 25, "Run"); --Botao group_n = 4
-    runButton:callback(RunCallback,{group:child(2),group:child(3)});
-    group:add(runButton);
-    scroll:add(comments); scroll:add(code); scroll:add(output); scroll:add(runButton);
+    scroll:callback(UpdateGui);
+    scroll:box("gtk up box");
+    window:add(scroll);
+    plusButton = fl.button((w/2)-100, h-200, 90, 25, "+");
+    plusButton:callback(Item);
+    window:add(plusButton)
 end
 
 Gui();
